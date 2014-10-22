7026787be761de49f00c8e76214f376fc6e7888c
Nikolay Raychev <Nikolay.G.Raychev@gmail.com>
2014-10-23 01:28:50 +0300

    gallery.php -> hover effect  for imgs

--- html5-template/gallery.php
+++ html5-template/gallery.php
@@ -25,12 +25,15 @@ my_header($title, $description, $keywords);
             <div class="row">
                <div class="col-md-4">
                   <h2>Heading</h2>
+                  	<div class="gallery-img">
+                  		<img src="images/jumbotron-background.png" />
+                  		<img src="images/jumbotron-background.png" />
+                  		<img src="images/jumbotron-background.png" />
+                  	</div>
                   <p>
                      Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
                   </p>
-                  <p>
-                     <a class="btn btn-default" href="#" role="button">View details &raquo;</a>
-                  </p>
+                  
                </div>
                <div class="col-md-4">
                   <h2>Heading</h2>
